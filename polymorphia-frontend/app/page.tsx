73fe0a13589055b0422588b0efcbfe5b255e844b
Kamil Rudny <k.rudny1@gmail.com>
2025-03-26 17:56:35 +0100

    Added test controller

--- polymorphia-frontend/app/page.tsx
+++ polymorphia-frontend/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import {fetchData} from "@/config";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchData<{ message: string }>("/test");
+
   return (
-    <div className="w-screen h-[100dvh] flex flex-col justify-center items-center">
-      <h1 className="text-6xl my-10">Polymorphia</h1>
-      <h2 className="text-xl text-center px-4">W nagrodę za dotarcie tutaj otrzymujesz Ananasa i 3.14 punktów!</h2>
-      <Image src="/pineapple.png" width="400" height="400" alt="Pineapple" />
-    </div>
+      <div className="w-screen h-[100dvh] flex flex-col justify-center items-center gap-y-8 px-4 text-center">
+        <h1 className="text-6xl">Polymorphia</h1>
+        <h2 className="text-xl">W nagrodę za dotarcie tutaj otrzymujesz Ananasa i 3.14 punktów!</h2>
+        <Image src="/pineapple.png" width="400" height="400" alt="Pineapple"/>
+        <h3 className="text-sm">{data.message ? data.message : "Loading..."}</h3>
+      </div>
   );
 }
