29f856ae0c9f6f30154ff2c1d6b23ba23f129b6b
ustankie <ustankiewicz@student.agh.edu.pl>
2025-04-29 22:56:09 +0200

    [OG-50] Move chestDtoToChest to mapper

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/RewardService.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/RewardService.java
@@ -4,14 +4,11 @@ import com.agh.polymorphia_backend.dto.request.course.reward.ChestRequestDto;
 import com.agh.polymorphia_backend.dto.request.course.reward.item.ItemRequestDto;
 import com.agh.polymorphia_backend.dto.response.course.reward.item.ItemResponseDto;
 import com.agh.polymorphia_backend.exception.database.InvalidArgumentException;
-import com.agh.polymorphia_backend.model.course.Course;
 import com.agh.polymorphia_backend.model.course.reward.Chest;
 import com.agh.polymorphia_backend.model.course.reward.item.Item;
-import com.agh.polymorphia_backend.repository.course.CourseRepository;
 import com.agh.polymorphia_backend.repository.course.reward.ChestRepository;
 import com.agh.polymorphia_backend.repository.course.reward.ItemRepository;
 import com.agh.polymorphia_backend.service.mapper.ChestMapper;
-import com.agh.polymorphia_backend.service.mapper.EvolutionStagesMapper;
 import com.agh.polymorphia_backend.service.mapper.ItemMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,30 +18,19 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class RewardService {
-    private static final String CHEST_NOT_FOUND = "Chests of ids [%s] not found";
+    public static final String CHEST_NOT_FOUND = "Chests of ids [%s] not found";
     private static final String ITEM_NOT_FOUND = "Items of ids [%s] not found";
 
     // repositories
-    private final CourseRepository courseRepository;
     private final ChestRepository chestRepository;
     private final ItemRepository<Item> itemRepository;
 
     // mappers
     private final ItemMapper itemMapper;
-    private final EvolutionStagesMapper evolutionStagesMapper;
     private final ChestMapper chestMapper;
 
     public Long addChest(ChestRequestDto chestRequestDto) {
-        Course course = courseRepository.findById(chestRequestDto.courseId())
-                .orElseThrow(() -> new InvalidArgumentException(CHEST_NOT_FOUND));
-        Chest chest = Chest.builder()
-                .course(course)
-                .behavior(chestRequestDto.behavior())
-                .name(chestRequestDto.name())
-                .imageUrl(chestRequestDto.imageUrl())
-                .description(chestRequestDto.description())
-                .build();
-        return chestRepository.save(chest).getId();
+        return chestRepository.save(chestMapper.chestRequestDtoToChest(chestRequestDto)).getId();
     }
 
     public Long addItem(ItemRequestDto itemRequestDto) {
--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/mapper/ChestMapper.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/mapper/ChestMapper.java
@@ -1,12 +1,22 @@
 package com.agh.polymorphia_backend.service.mapper;
 
+import com.agh.polymorphia_backend.dto.request.course.reward.ChestRequestDto;
 import com.agh.polymorphia_backend.dto.response.course.reward.ChestResponseDto;
+import com.agh.polymorphia_backend.exception.database.InvalidArgumentException;
+import com.agh.polymorphia_backend.model.course.Course;
 import com.agh.polymorphia_backend.model.course.reward.Chest;
 import com.agh.polymorphia_backend.model.course.reward.item.Item;
+import com.agh.polymorphia_backend.repository.course.CourseRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.agh.polymorphia_backend.service.RewardService.CHEST_NOT_FOUND;
+
 @Service
+@AllArgsConstructor
 public class ChestMapper {
+
+    private final CourseRepository courseRepository;
 
     public ChestResponseDto chestToChestResponseDto(Long courseId, Chest chest) {
         return ChestResponseDto.builder()
@@ -17,6 +27,18 @@ public class ChestMapper {
                 .behavior(chest.getBehavior().getTextValue())
                 .description(chest.getDescription())
                 .itemIds(chest.getItems().stream().map(Item::getId).toList())
+                .build();
+    }
+
+    public Chest chestRequestDtoToChest(ChestRequestDto chestRequestDto) {
+        Course course = courseRepository.findById(chestRequestDto.courseId())
+                .orElseThrow(() -> new InvalidArgumentException(CHEST_NOT_FOUND));
+        return Chest.builder()
+                .course(course)
+                .behavior(chestRequestDto.behavior())
+                .name(chestRequestDto.name())
+                .imageUrl(chestRequestDto.imageUrl())
+                .description(chestRequestDto.description())
                 .build();
     }
 }
