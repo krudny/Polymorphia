0f9a53392f9b4af3008f42dd8f21bf2202401851
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-05-04 17:34:45 +0200

    add static file server

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/config/WebConfig.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/config/WebConfig.java
@@ -2,10 +2,17 @@ package com.agh.polymorphia_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final StaticFileServerProperties staticFileServerProperties;
+
+    public WebConfig(StaticFileServerProperties staticFileServerProperties) {
+        this.staticFileServerProperties = staticFileServerProperties;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -18,5 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("file:" + staticFileServerProperties.rootDir());
     }
 }
