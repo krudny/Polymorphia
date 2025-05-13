660a8294856a72bb475b2cb75fb5252a07ff9d71
Katarzyna Lisiecka-Meller <51798453+LisieckaK00@users.noreply.github.com>
2025-05-13 11:32:54 +0200

    [OG-52] Spring security - basics (#7)
    * security introduced
    
    * after review changes
    
    * add tanstack form and zod, update login form
    
    * api_host update
    
    * showing errors update, cosmetic changes
    
    * cors update
    
    * added initial user (coordinator)
    
    * remove password min error

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/UserService.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/UserService.java
@@ -7,17 +7,24 @@ import com.agh.polymorphia_backend.model.user.User;
 import com.agh.polymorphia_backend.repository.user.UserRepository;
 import com.agh.polymorphia_backend.service.mapper.UserMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserService {
-    private static final String USER_NOT_FOUND = "User of id %s does not exist in the database";
+public class UserService implements UserDetailsService {
+    private static final String USER_NOT_FOUND = "User %s does not exist in the database";
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     public Long addUser(UserRequestDto userRequestDto) {
         User user = userMapper.userRequestDtoToUser(userRequestDto);
+        String encodedPassword = passwordEncoder.encode(userRequestDto.getPassword());
+        user.setPassword(encodedPassword);
         return userRepository.save(user).getId();
     }
 
@@ -25,5 +32,10 @@ public class UserService {
         User user = userRepository.findById(id).orElseThrow(() ->
                 new InvalidArgumentException(String.format(USER_NOT_FOUND, id)));
         return userMapper.userToUserResponseDto(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
     }
 }
--- polymorphia-frontend/components/home/LoginForm.tsx
+++ polymorphia-frontend/components/home/LoginForm.tsx
@@ -1,61 +1,110 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
+import React from "react";
+import { useForm } from "@tanstack/react-form";
+import { useMutation } from "@tanstack/react-query";
 import ButtonWithBorder from "@/components/button/ButtonWithBorder";
 import NavigationArrow from "@/components/slider/NavigationArrow";
 import LoginFormProps from "@/interfaces/home/LoginFormInterface";
-import "../../styles/home.css"
+import { LoginDto } from "@/interfaces/api/DTO";
+import { FieldInfo } from "@/components/form/FieldInfo";
+import { loginSchema } from "@/components/form/schema";
+import AuthService from "@/services/AuthService";
+import "../../styles/home.css";
 
 export default function LoginForm({ onBackAction }: LoginFormProps) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    } as LoginDto,
+    validators: {
+      onBlur: loginSchema
+    },
+    onSubmit: async ({ value }) => {
+      loginMutation.mutate(value);
+    },
+  });
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    console.log('Email:', email);
-    console.log('Hasło:', password);
-  };
+  const loginMutation = useMutation({
+    mutationFn: AuthService.login,
+    onSuccess: () => {
+      alert("Zalogowano!");
+      form.reset();
+    },
+    onError: (error: Error) => {
+      alert(error.message);
+    },
+  });
 
   return (
-      <div className="login-wrapper">
-        <NavigationArrow direction="left" onClick={onBackAction} className="cursor-pointer left-0"/>
-        <div>
-          <form onSubmit={handleSubmit} className="login-form">
-            <div>
-              <input
+    <div className="login-wrapper">
+      <NavigationArrow
+        direction="left"
+        onClick={onBackAction}
+        className="cursor-pointer left-0"
+      />
+      <div>
+        <form
+          className="login-form"
+          onSubmit={(event: React.FormEvent) => {
+            event.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <form.Field name="email">
+            {(field) => (
+              <div>
+                <input
                   type="email"
-                  id="email"
+                  id={field.name}
                   placeholder="Twój email"
-                  value={email}
-                  onChange={handleEmailChange}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
                   required
                   autoComplete="off"
-              />
-            </div>
-            <div>
-              <input
+                />
+                <FieldInfo field={field} />
+              </div>
+            )}
+          </form.Field>
+
+          <form.Field name="password">
+            {(field) => (
+              <div>
+                <input
                   type="password"
-                  id="password"
+                  id={field.name}
                   placeholder="Twoje hasło"
-                  value={password}
-                  onChange={handlePasswordChange}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
                   required
-              />
-            </div>
-            <ButtonWithBorder text="Zaloguj się" className="mt-12"/>
-          </form>
-          <div className="forgot-password">
-            <p>Zapomniałeś hasła?</p>
-          </div>
+                />
+                <FieldInfo field={field} />
+              </div>
+            )}
+          </form.Field>
+
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isPristine]}
+          >
+            {([canSubmit, isPristine]) => (
+              <>
+                <ButtonWithBorder
+                  text="Zaloguj się"
+                  className="mt-12"
+                  isActive={isPristine || !canSubmit}
+                />
+              </>
+            )}
+          </form.Subscribe>
+        </form>
+        <div className="forgot-password">
+          <p>Zapomniałeś hasła?</p>
         </div>
       </div>
+    </div>
   );
 }
