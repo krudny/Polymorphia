72e7ae570bafeb67d0c43f36147f563be75ade34
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-05-22 17:26:55 +0200

    structure for event sections

---
+++ polymorphia-frontend/interfaces/course/EventSectionInterfaces.ts
@@ -0,0 +1,24 @@
+export interface EventSectionCore {
+    id: number;
+    name: string;
+}
+
+export interface EventSection {
+    id: number;
+    type: "coursework" | "tests" | "project";
+    name: string;
+    gainedXp: number;
+    flatBonusXp: number;
+    percentageBonus: number;
+    percentageBonusXp: number;
+    totalXp: number;
+    gradableEvents: GradableEventCore[];
+}
+
+export interface GradableEventCore {
+    id: number;
+    name: string;
+    topic?: string;
+    gainedXp?: number; // need to decide now to handle no grade
+    hidden?: boolean;
+}
