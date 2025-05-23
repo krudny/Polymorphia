80fcdf5a947244d8aeec55ed0a7eea446a2f23b6
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2025-05-23 16:46:45 +0200

    [OG-62] Progress bar (#12)
    * [OG-62] Progress bar somehow works
    
    * [OG-62] Progress bar works
    
    * [OG-62] Code refactor
    
    * [OG-62] Update gitignore

--- polymorphia-frontend/app/(logged-in)/profile/page.tsx
+++ polymorphia-frontend/app/(logged-in)/profile/page.tsx
@@ -3,33 +3,57 @@ import Image from "next/image";
 import {useScaleShow} from "@/animations/General";
 import {API_STATIC_URL} from "@/services/api";
 import "../../../styles/profile.css"
+import ProgressBar from "@/components/progressbar/ProgressBar";
 
 export default function Profile() {
   const wrapperRef = useScaleShow();
 
   return (
       <div ref={wrapperRef} className="profile">
-        <div className="profile-content-wrapper">
-            <div className="profile-image-wrapper">
-              <Image
-                  src={`${API_STATIC_URL}/images/evolution-stages/4.jpg`}
-                  alt="User profile"
-                  fill
-                  className="profile-img"
-                  priority
-                  sizes="(min-width: 1024px) 25vw, 50vw"
-              />
+          <div className="profile-wrapper">
+            <div className="profile-content-wrapper">
+                <div className="profile-image-wrapper">
+                  <Image
+                      src={`${API_STATIC_URL}/images/evolution-stages/4.jpg`}
+                      alt="User profile"
+                      fill
+                      className="profile-img"
+                      priority
+                      sizes="(min-width: 1024px) 25vw, 50vw"
+                  />
+                </div>
+              <div className="profile-content">
+                <h1>Kamil Rudny</h1>
+                <h2>Gerard (65xp)</h2>
+                <h3>Jesteś 36 na 139 zwierzaków!</h3>
+              </div>
             </div>
-          <div className="profile-content">
-            <h1>Kamil Rudny</h1>
-            <h2>Gerard (65xp)</h2>
-            <h3>Jesteś 36 na 139 zwierzaków!</h3>
-          </div>
-        </div>
-        <div className="profile-progress-bar">
-          <h1>Duży pasek postępu</h1>
-        </div>
+            <div className="profile-progress-bar-mobile">
+                <ProgressBar
+                    minXP={60}
+                    currentXP={65}
+                    maxXP={70}
+                    numSquares={2}
+                    segmentSizes={[0, 100, 0]}
+                    upperTextLabels={["3.5 (60xp)", "4.0 (70xp)"]}
+                    bottomTextLabels={["Żółtodziób", "Nieopierzony Odkrywca"]}
+                    labelsSize="sm"
+                />
+            </div>
 
+            <div className="profile-progress-bar-desktop">
+                <ProgressBar
+                    minXP={0}
+                    currentXP={65}
+                    maxXP={100}
+                    numSquares={8}
+                    segmentSizes={[0, 25, 0, 25, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0 ]}
+                    upperTextLabels={["2.0 (0xp)", "2.0 (25xp)", "3.0 (50xp)", "3.5 (60xp)", "4.0 (70xp)", "4.5 (80xp)", "5.0 (90xp)", "5.0 (100xp)"]}
+                    bottomTextLabels={["Jajo", "Pisklak", "Podlot", "Żółtodziób", "Nieopierzony Odkrywca", "Samodzielny Zwierzak", "Majestatyczna Bestia", "Władca Polymorphii"]}
+                    labelsSize="sm"
+                />
+            </div>
+          </div>
       </div>
   )
 }
---
+++ polymorphia-frontend/components/progressbar/ProgressBarSquare.tsx
@@ -0,0 +1,30 @@
+import {SquareFillCalc} from "@/components/progressbar/ProgressBarUtil";
+import "../../styles/progressbar.css"
+
+export default function ProgressBarSquare({ squareFill, position }: { squareFill: number, position: number }) {
+    return (
+        <>
+            <div className="progressbar-square" style={{ left: `${position}%` }}>
+                <div
+                    className="bg-neutral-400"
+                    style={{
+                        transform: 'rotate(45deg)',
+                        transformOrigin: 'center center',
+                    }}
+                />
+            </div>
+
+
+            <div className="progressbar-square" style={{ left: `${position}%` }}>
+                <div
+                    className="bg-neutral-800"
+                    style={{
+                        transform: 'rotate(45deg)',
+                        transformOrigin: 'center center',
+                        clipPath: SquareFillCalc(squareFill),
+                    }}
+                />
+            </div>
+        </>
+    )
+}
