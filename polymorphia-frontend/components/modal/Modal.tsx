c37e5dcb7670a6b08d8174fe4852a2ee85cdbdd7
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-05-23 22:36:18 +0200

    modal cleanup

--- polymorphia-frontend/components/modal/Modal.tsx
+++ polymorphia-frontend/components/modal/Modal.tsx
@@ -1,8 +1,8 @@
 import { ModalProps } from '@/interfaces/modal/ModalInterfaces';
 import clsx from 'clsx';
 import { X } from 'lucide-react';
-import Image from 'next/image';
 import { createPortal } from 'react-dom';
+import '../../styles/modal.css';
 
 export default function Modal({
   isOpen,
@@ -13,22 +13,20 @@ export default function Modal({
   return createPortal(
     <div
       className={clsx(
-        'fixed top-0 left-0 flex items-center justify-center w-screen h-screen z-[51] bg-neutral-800/50 backdrop-blur-xs transition-opacity',
-        isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        'modal-backdrop',
+        isOpen ? 'modal-visible' : 'modal-not-visible'
       )}
       onClick={isOpen ? onClose : undefined}
     >
       <div
-        className={clsx(
-          'm-3 w-sm lg:w-md 2xl:w-lg min-h-32 p-3 md:p-5 z-[52] bg-neutral-300 border-2 border-neutral-800 rounded-xl transition-opacity',
-        )}
+        className="modal"
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
-        <div className="flex flex-row justify-between items-start mb-2">
-          <h1 className="text-4xl">{title}</h1>
-          <X className="cursor-pointer hover:drop-shadow-lg" onClick={onClose}/>
+        <div className="modal-header">
+          <h1>{title}</h1>
+          <X className="modal-header-exit-button" onClick={onClose} />
         </div>
         {children}
       </div>
