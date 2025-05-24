273c83539f5855bf2894b736739354ad4be787eb
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-05-24 15:35:35 +0200

    bonus modal

--- polymorphia-frontend/components/course/event-section/points-summary/BonusInfoModal.tsx
+++ polymorphia-frontend/components/course/event-section/points-summary/BonusInfoModal.tsx
@@ -1,8 +1,22 @@
-import Modal from "@/components/modal/Modal";
-import { BonusInfoModalProps } from "@/interfaces/course/PointsSummaryInterfaces";
+import Modal from '@/components/modal/Modal';
+import { BonusInfoModalProps } from '@/interfaces/course/PointsSummaryInterfaces';
+import BonusItemCard from './BonusItemCard';
 
-export default function BonusInfoModal({ bonusInfo, onClose}: BonusInfoModalProps) {
-  return <Modal isOpen={bonusInfo !== null} title={bonusInfo?.name ?? ''} onClose={onClose}>
-    Hi!
-  </Modal>
+export default function BonusInfoModal({
+  bonusInfo,
+  onClose,
+}: BonusInfoModalProps) {
+  return (
+    <Modal
+      isOpen={bonusInfo !== null}
+      title={bonusInfo?.name ?? ''}
+      onClose={onClose}
+    >
+      <div className="flex flex-col gap-2">
+        {bonusInfo?.items.map((item) => (
+          <BonusItemCard key={item.assignedId} item={item} />
+        ))}
+      </div>
+    </Modal>
+  );
 }
