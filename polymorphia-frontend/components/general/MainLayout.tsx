28ef71a07a7b662769eadb8698395713801ab5b1
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-06-01 13:23:48 +0200

    prevent navbar submenu animation after click

---
+++ polymorphia-frontend/components/general/MainLayout.tsx
@@ -0,0 +1,22 @@
+import { useScaleShow } from '@/animations/General';
+import { useTitle } from '../navigation/TitleContext';
+import { ReactNode } from 'react';
+
+export default function MainLayout({ children }: { children: ReactNode }) {
+  const { title } = useTitle();
+  const titleRef = useScaleShow();
+
+  return (
+    <div
+      id="main-container"
+      className="w-full h-full flex flex-col min-h-[calc(100dvh-5rem)] lg:h-screen relative overflow-hidden lg:overflow-y-auto custom-scrollbar max-lg:mt-25"
+    >
+      <div className="w-full flex-centered h-15 shrink-0 hidden lg:flex">
+        <h3 className="text-4xl" ref={titleRef}>
+          {title}
+        </h3>
+      </div>
+      {children}
+    </div>
+  );
+}
