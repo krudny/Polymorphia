4ae7574a9b79ce64ac5dfc03cf9a63d2a1851c7b
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-06-01 14:02:13 +0200

    experimental: add minimal delay to useScaleShow

--- polymorphia-frontend/animations/General.tsx
+++ polymorphia-frontend/animations/General.tsx
@@ -1,28 +1,35 @@
-"use client"
-import gsap from "gsap";
-import {useEffect, useRef} from "react";
+'use client';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
 
 export const animateScaleShow = (ref: HTMLDivElement) => {
-  gsap.fromTo(ref, {
-    scale: 0.9,
-    autoAlpha: 0
-  }, {
-    scale: 1,
-    autoAlpha: 1,
-    duration: 0.3,
-    ease: "power1.in"
-  });
-}
-
+  gsap.fromTo(
+    ref,
+    {
+      scale: 0.9,
+      autoAlpha: 0,
+    },
+    {
+      scale: 1,
+      autoAlpha: 1,
+      duration: 0.3,
+      ease: 'power1.in',
+    }
+  );
+};
 
 export function useScaleShow() {
-  const ref = useRef<HTMLDivElement | null>(null)
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (ref.current) {
-      animateScaleShow(ref.current)
-    }
-  }, [])
+    const handle = setTimeout(() => {
+      if (ref.current) {
+        animateScaleShow(ref.current);
+      }
+    }, 10);
 
-  return ref
+    return () => clearTimeout(handle);
+  }, []);
+
+  return ref;
 }
