2d0306b9bb06fa3716bf8e9962da1e4d40be0481
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-06-14 21:28:42 +0200

    merge Modal and ModalInner

--- polymorphia-frontend/components/modal/Modal.tsx
+++ polymorphia-frontend/components/modal/Modal.tsx
@@ -1,10 +1,21 @@
 import { ModalProps } from "@/interfaces/modal/ModalInterfaces";
-import ModalInner from "./ModalInner";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ModalProvider } from "../providers/ModalContext";
+import { useAnimatedModalState } from "@/animations/Modal";
+import { createPortal } from "react-dom";
+import clsx from "clsx";
+import { X } from "lucide-react";
+import "../../styles/modal.css";
 
-export default function Modal(props: ModalProps) {
-  const { isDataPresented, onClosed, ...rest } = props;
+export default function Modal({
+  isDataPresented,
+  onClosed,
+  title,
+  subtitle,
+  children,
+}: ModalProps) {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+  const backdropRef = useRef<HTMLDivElement | null>(null);
 
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -12,18 +23,48 @@ export default function Modal(props: ModalProps) {
     setModalVisible(isDataPresented);
   }, [isDataPresented]);
 
+  const modalState = useAnimatedModalState(
+    modalVisible,
+    modalRef,
+    backdropRef,
+    onClosed
+  );
+
   const onRequestClose = () => {
     setModalVisible(false);
   };
 
-  return (
+  return createPortal(
     <ModalProvider closeModal={onRequestClose}>
-      <ModalInner
-        isOpen={modalVisible}
-        onRequestClose={onRequestClose}
-        onClosed={onClosed}
-        {...rest}
-      />
-    </ModalProvider>
+      <div
+        ref={backdropRef}
+        className={clsx(
+          "modal-backdrop",
+          modalState !== "closed" ? "modal-visible" : "modal-not-visible"
+        )}
+        onClick={onRequestClose}
+      >
+        <div
+          ref={modalRef}
+          className="modal overflow-auto"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <div className="modal-header-wrapper">
+            <div className="modal-header">
+              <h1>{title}</h1>
+              <X
+                className="modal-header-exit-button"
+                onClick={onRequestClose}
+              />
+            </div>
+            <h2>{subtitle}</h2>
+          </div>
+          {children}
+        </div>
+      </div>
+    </ModalProvider>,
+    document.body
   );
 }
