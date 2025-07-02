86a20df8155dd9f92b52c30abe9f6535db0e49c2
Kamil Rudny <kamil.rudny@schibsted.com>
2025-07-02 22:18:41 +0200

    [OG-6] Implemented hall of fame reducer

--- polymorphia-frontend/components/providers/HallOfFameContext.tsx
+++ polymorphia-frontend/components/providers/HallOfFameContext.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import {createContext, ReactNode, useEffect, useState} from "react";
+import {createContext, ReactNode, useEffect, useReducer, useState} from "react";
 import {useDebounce} from "use-debounce";
 import {useQuery} from "@tanstack/react-query";
 import {EventSectionService} from "@/services/course/event-section/EventSectionService";
 import {addEventSectionsToCategories, getAllCategories} from "@/services/hall-of-fame/Helpers";
 import {filtersCategories} from "@/services/hall-of-fame/FilterCategories";
-
+import {HallOfFameReducer} from "@/services/hall-of-fame/HallOfFameReducer";
 
 export const HallOfFameContext = createContext({
   page: 0,
@@ -22,8 +22,9 @@ export const HallOfFameProvider = ({children} : {children:ReactNode}) => {
   const [search, setSearch] = useState("");
   const [debouncedSearch] = useDebounce(search, 400);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [filtersState, setFiltersState] = useState(filtersCategories);
-  const { sortByCategory, rankingOptionsCategory} = getAllCategories(filtersState);
+  const [filtersState, setFiltersState] = useReducer(HallOfFameReducer, filtersCategories);
+  const [appliedFiltersState, setAppliedFiltersState] = useState(filtersState);
+  const { sortByCategory, rankingOptionsCategory } = getAllCategories(filtersState);
 
   const { data: eventSections } = useQuery({
     queryKey: ["eventSections"],
@@ -36,7 +37,6 @@ export const HallOfFameProvider = ({children} : {children:ReactNode}) => {
       addEventSectionsToCategories(eventSections, rankingOptionsCategory);
     }
   }, [eventSections, sortByCategory, rankingOptionsCategory]);
-
 
   <HallOfFameContext.Provider
     value={{
---
+++ polymorphia-frontend/services/hall-of-fame/HallOfFameReducer.ts
@@ -0,0 +1,72 @@
+export function HallOfFameReducer(filters: any, action: any) {
+    switch (action.type) {
+        case "TOGGLE_SORT_ORDER":
+            return filters.map(filter => {
+                if (filter.id !== action.payload.id) return filter;
+
+                const updatedOptions = filter.options.map(option => ({
+                    ...option,
+                    isSelected: !option.isSelected,
+                }));
+
+                return {
+                    ...filter,
+                    options: updatedOptions,
+                };
+            });
+
+        case "CLOSE_ALL_CATEGORIES":
+            return filters.map(filter => ({
+                ...filter,
+                isOpen: false,
+            }));
+
+        case "ADD_CATEGORY_SELECTION":
+            return filters.map(filter => {
+                if (filter.id !== action.payload.id) return filter;
+
+                let updatedOptions;
+
+                if (action.payload.value === "all") {
+                    updatedOptions = filter.options.map(option => ({
+                        ...option,
+                        isSelected: option.value === "all",
+                    }));
+                } else {
+                    updatedOptions = filter.options.map(option => {
+                        if (option.value === action.payload.value) {
+                            return { ...option, isSelected: true };
+                        } else if (option.value === "all") {
+                            return { ...option, isSelected: false };
+                        }
+                        return option;
+                    });
+                }
+
+                return {
+                    ...filter,
+                    options: updatedOptions,
+                };
+            });
+
+        case "REMOVE_CATEGORY_SELECTION":
+            return filters.map(filter => {
+                if (filter.id !== action.payload.id) return filter;
+
+                const updatedOptions = filter.options.map(option =>
+                    option.value === action.payload.value
+                        ? { ...option, isSelected: false }
+                        : option
+                );
+
+                return {
+                    ...filter,
+                    options: updatedOptions,
+                }
+            })
+
+
+        default:
+            return filters;
+    }
+}
