3dc57867d05df2532ec9529eb46ff965ce6fc15f
Kamil Rudny <kamil.rudny@schibsted.com>
2025-07-13 22:18:15 +0200

    [OG-21] Background fix

--- polymorphia-frontend/app/(logged-in)/layout.tsx
+++ polymorphia-frontend/app/(logged-in)/layout.tsx
@@ -3,10 +3,10 @@
 import { ReactNode } from "react";
 import Navigation from "@/components/navigation/Navigation";
 import { NavigationProvider } from "@/components/providers/navigation/NavigationContext";
-import Image from "next/image";
-import MainLayout from "@/components/main-layout/MainLayout";
 import { UserProvider } from "@/components/providers/user/UserContext";
 import { useTheme } from "next-themes";
+import BackgroundWrapper from "@/components/background-wrapper/BackgroundWrapper";
+import MainLayout from "@/components/main-layout/MainLayout";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const { resolvedTheme } = useTheme();
@@ -18,14 +18,15 @@ export default function Layout({ children }: { children: ReactNode }) {
       <NavigationProvider>
         <div className="w-full min-h-[100dvh] relative flex flex-col lg:flex-row bg-black">
           <Navigation />
-          <Image
-            src={`/${url}`}
-            alt="Background"
-            fill
-            className="absolute object-cover"
-            priority
-            sizes="100%"
-          />
+          {/*<Image*/}
+          {/*  src={`/${url}`}*/}
+          {/*  alt="Background"*/}
+          {/*  fill*/}
+          {/*  className="absolute object-cover"*/}
+          {/*  priority*/}
+          {/*  sizes="100%"*/}
+          {/*/>*/}
+          <BackgroundWrapper />
           <MainLayout>{children}</MainLayout>
         </div>
       </NavigationProvider>
