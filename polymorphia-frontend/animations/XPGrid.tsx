4126d36ff2d84afd670c49ee0044eefbf9318a26
Kamil Rudny <kamil.rudny@schibsted.com>
2025-07-15 23:38:27 +0200

    [OG-21] Half code refactor, animation fix

--- polymorphia-frontend/animations/XPGrid.tsx
+++ polymorphia-frontend/animations/XPGrid.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { GradableEventResponseDTO } from "@/app/(logged-in)/course/[eventSectionType]/BetterEventSectionService";
+import { GradableEventResponseDTO } from "@/app/(logged-in)/course/BetterEventSectionService";
 
 export function useEventSectionAnimation(
   pageToShow: number,
--- polymorphia-frontend/components/course/project-section/modals/EventRewardModal.tsx
+++ polymorphia-frontend/components/course/project-section/modals/EventRewardModal.tsx
@@ -15,7 +15,11 @@ export default function AssignmentDetailsModal({
   isError: boolean;
 }) {
   return (
-    <Modal isDataPresented={true} onClosed={onClosed} title="Nagrody">
+    <Modal
+      isDataPresented={data !== undefined}
+      onClosed={onClosed}
+      title="Nagrody"
+    >
       {isError && (
         <div className="gradable-event-section text-xl 2xl:text-2xl">
           Wystąpił błąd przy ładowaniu szczegółów.
