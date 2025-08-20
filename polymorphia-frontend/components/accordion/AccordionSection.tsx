9c859a45f749290c737624538c4c88371627d6eb
kligarski <63918941+kligarski@users.noreply.github.com>
2025-08-20 21:42:37 +0200

    [OG-85] accordion frontend component (#79)
    * add test page
    
    * Accordion PoC
    
    * animation
    
    * add initiallyOpened prop
    
    * programmatic control
    
    * refactor
    
    * apply suggestions
    
    * add closeAll
    
    * use Accordion in FiltersModal
    
    * change maxOpen to number | undefined
    
    * remove test-accordion example page

---
+++ polymorphia-frontend/components/accordion/AccordionSection.tsx
@@ -0,0 +1,57 @@
+import { animateAccordion } from "@/animations/Accordion";
+import clsx from "clsx";
+import { ArrowDown, ArrowUp } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { useAccordion } from "../providers/accordion/AccordionContext";
+import "./index.css";
+import { AccordionSectionProps } from "./types";
+
+export default function AccordionSection({
+  id,
+  title,
+  children,
+  isInitiallyOpened,
+  headerClassName,
+}: AccordionSectionProps) {
+  const { register, unregister, isOpen, toggle, open } = useAccordion();
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    register(id);
+    if (isInitiallyOpened) {
+      open(id);
+    }
+    return () => unregister(id);
+  }, [id, isInitiallyOpened, open, register, unregister]);
+
+  const isOpened = isOpen(id);
+  useEffect(() => {
+    if (contentRef.current) {
+      animateAccordion(contentRef.current, isOpened);
+    }
+  }, [isOpened]);
+
+  return (
+    <div className="accordion-section">
+      <div className={clsx("accordion-section-header", headerClassName)}>
+        <h1>{title}</h1>
+        <div className="accordion-toggle-button" onClick={() => toggle(id)}>
+          {isOpened ? (
+            <div className="accordion-toggle">
+              <ArrowUp className="accordion-toggle-arrow" />
+              <h2>Zamknij</h2>
+            </div>
+          ) : (
+            <div className="accordion-toggle">
+              <ArrowDown className="accordion-toggle-arrow" />
+              <h2>Otwórz</h2>
+            </div>
+          )}
+        </div>
+      </div>
+      <div ref={contentRef} className="accordion-section-content">
+        {children}
+      </div>
+    </div>
+  );
+}
