c65fe99e3f3b8cfa5c388cec7099f2cbf730edfa
Kamil Rudny <k.rudny1@gmail.com>
2025-09-09 23:23:36 +0200

    [OG-90] Now js is not compiling

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/controller/CSVController.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/controller/CSVController.java
@@ -3,6 +3,7 @@ package com.agh.polymorphia_backend.controller;
 import com.agh.polymorphia_backend.service.csv.CSVService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,7 +15,7 @@ import java.util.List;
 public class CSVController {
     private CSVService csvService;
 
-    @GetMapping("/preview")
+    @PostMapping("/preview")
     public List<String[]> importCSV(@RequestParam MultipartFile file, @RequestParam String type) {
         return csvService.readCSV(file, type);
     }
