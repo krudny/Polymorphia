8d8c9a51588a98952003c9524dfb9f325c1efee8
Kamil Rudny <k.rudny1@gmail.com>
2025-09-14 12:41:27 +0200

    [OG-90] Remove unused method

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/csv/CSVUtil.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/service/csv/CSVUtil.java
@@ -1,12 +1,5 @@
 package com.agh.polymorphia_backend.service.csv;
 
-import org.mozilla.universalchardet.UniversalDetector;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class CSVUtil {
@@ -14,35 +7,10 @@ public class CSVUtil {
         return headers.indexOf(columnName);
     }
 
-    public static Charset detectCharset(MultipartFile file) {
-        try (InputStream inputStream = file.getInputStream()) {
-            UniversalDetector detector = new UniversalDetector(null);
-
-            byte[] buf = new byte[4096];
-            int nread;
-            while ((nread = inputStream.read(buf)) > 0 && !detector.isDone()) {
-                detector.handleData(buf, 0, nread);
-            }
-            detector.dataEnd();
-
-            String encoding = detector.getDetectedCharset();
-            detector.reset();
-
-            if (encoding != null) {
-                if ("WINDOWS-1252".equals(encoding)) {
-                    return Charset.forName("windows-1250");
-                }
-
-                return Charset.forName(encoding);
-            }
-            return StandardCharsets.UTF_8;
-        } catch (IOException e) {
-            return StandardCharsets.UTF_8;
-        }
-    }
-
     public static boolean isValidEncoding(String[] headers) {
-        if (headers == null) return false;
+        if (headers == null) {
+            return false;
+        }
 
         String combined = String.join(" ", headers);
 
