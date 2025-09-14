664596b19f4e07fdc04865476e9e954cd7af6c44
Kamil Rudny <k.rudny1@gmail.com>
2025-09-14 20:12:05 +0200

    [FIX]

--- polymorphia-backend/src/test/java/com/agh/polymorphia_backend/controller/ControllerTestConfig.java
+++ polymorphia-backend/src/test/java/com/agh/polymorphia_backend/controller/ControllerTestConfig.java
@@ -36,6 +36,7 @@ public abstract class ControllerTestConfig {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
     }
 
     @BeforeEach
