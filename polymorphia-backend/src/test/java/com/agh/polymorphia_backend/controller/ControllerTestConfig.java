3d193f1d5e9a30b92f7b60ef37ae01aa9c3960bc
Kamil Rudny <k.rudny1@gmail.com>
2025-09-16 20:53:46 +0200

    test

--- polymorphia-backend/src/test/java/com/agh/polymorphia_backend/controller/ControllerTestConfig.java
+++ polymorphia-backend/src/test/java/com/agh/polymorphia_backend/controller/ControllerTestConfig.java
@@ -37,10 +37,10 @@ public abstract class ControllerTestConfig {
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
 
-//        registry.add("initial.user.firstName", () -> "Demo");
-//        registry.add("initial.user.lastName", () -> "User");
-//        registry.add("initial.user.email", () -> "demo@test.com");
-//        registry.add("initial.user.password", () -> "testPassword");
+       registry.add("initial.user.firstName", () -> "Demo");
+       registry.add("initial.user.lastName", () -> "User");
+       registry.add("initial.user.email", () -> "demo@test.com");
+       registry.add("initial.user.password", () -> "testPassword");
     }
 
     @BeforeEach
