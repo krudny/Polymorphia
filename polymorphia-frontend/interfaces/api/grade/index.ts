0ee07c6e0167c748b5a6cfcf69bbcd8a43eaa8c5
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-09-17 20:29:36 +0200

    new grading interfaces

--- polymorphia-frontend/interfaces/api/grade/index.ts
+++ polymorphia-frontend/interfaces/api/grade/index.ts
@@ -1,36 +1,29 @@
 import { RewardResponseDTO } from "../reward";
-import { AssignedRewardResponseDTO } from "../reward/assigned";
 
-// TODO: to sie zwraca jak jest ocena
-export interface GradeResponseDTO {
-  details: GradeDetailsResponseDTO;
-  criteria: CriterionGradeResponseDTO[];
+// Grade
+export interface ShortAssignedRewardResponseDTO {
+  id: number;
+  imageUrl: string;
+  quantity: number;
 }
 
-export interface ShortGradeResponseDTO {
-  comment: string;
-  criteria: {
-    id: number;
-    gainedXp: number;
-    assignedRewards: {
-      id: number;
-      quantity: number;
-      imageUrl: string;
-    }[];
-  }[];
-}
-
-export interface GradeDetailsResponseDTO {
+export interface GradeResponseDTO<AssignedRewardType> {
   id: number;
   comment: string;
+  criteria: CriterionGradeResponseDTO<AssignedRewardType>[];
 }
 
+export type ShortGradeResponseDTO =
+  GradeResponseDTO<ShortAssignedRewardResponseDTO>;
+
+// export type FullGradeResponseDTO = GradeResponseDTO<AssignedRewardResponseDTO>;
+
+// Criteria
 export interface CriterionResponseDTO {
   id: number;
   name: string;
   maxXp: string;
   assignableRewards: CriterionAssignableRewardResponseDTO[];
-  criterionGrade?: CriterionGradeResponseDTO; // TODO: to do usuniecia
 }
 
 export interface CriterionAssignableRewardResponseDTO {
@@ -38,8 +31,33 @@ export interface CriterionAssignableRewardResponseDTO {
   maxAmount: number;
 }
 
-export interface CriterionGradeResponseDTO {
+export interface CriterionGradeResponseDTO<AssignedRewardType> {
   id: number;
   gainedXp: string;
-  assignedRewards: AssignedRewardResponseDTO[];
+  assignedRewards: AssignedRewardType[];
 }
+
+// GradingTarget
+interface StudentTargetData {
+  id: number;
+  studentName: string;
+  evolutionStage: string;
+  group: string;
+  imageUrl: string;
+  gainedXp: string;
+}
+
+export interface StudentTargetResponseDTO extends StudentTargetData {
+  type: "STUDENT";
+}
+
+export interface StudentGroupTargetResponseDTO {
+  type: "STUDENT_GROUP";
+  groupType: "MATCHING" | "DIVERGENT";
+  groupId: number;
+  members: StudentTargetData[];
+}
+
+export type TargetResponseDTO =
+  | StudentTargetResponseDTO
+  | StudentGroupTargetResponseDTO;
