85644d32522ffa6a8be01ab2a27cf5d2678a712f
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2025-09-21 13:48:22 +0200

    [OG-90] Import csv (#103)

--- polymorphia-frontend/components/speed-dial/strategies/instructor/TestGrading.tsx
+++ polymorphia-frontend/components/speed-dial/strategies/instructor/TestGrading.tsx
@@ -1,12 +1,13 @@
-import { BaseSpeedDialStrategy } from "@/components/speed-dial/strategies/Base";
-import { SpeedDialStrategy } from "@/components/speed-dial/strategies/types";
-import { SpeedDialItem } from "@/components/speed-dial/types";
+import {BaseSpeedDialStrategy} from "@/components/speed-dial/strategies/Base";
+import {SpeedDialStrategy} from "@/components/speed-dial/strategies/types";
+import {SpeedDialItem} from "@/components/speed-dial/types";
+import {ImportCSVTypes} from "@/interfaces/general";
 
 export class TestGradingStrategy
   extends BaseSpeedDialStrategy
   implements SpeedDialStrategy
 {
   getItems(): SpeedDialItem[] {
-    return [this.createImportCSV()];
+    return [this.createImportCSV(ImportCSVTypes.GRADE_IMPORT)];
   }
 }
---
+++ polymorphia-frontend/hooks/contexts/useImportCSVContext/index.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { ImportCSVContextInterface } from "@/providers/import-csv/types";
+import { ImportCSVContext } from "@/providers/import-csv";
+
+export default function useImportCSVContext(): ImportCSVContextInterface {
+  const context = useContext(ImportCSVContext);
+
+  if (!context) {
+    throw new Error(
+      "useImportCSVContext must be used within ImportCSVProvider"
+    );
+  }
+
+  return context;
+}
