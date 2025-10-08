9914a8ee9a79545a5455dcac96bb4a4e01f645a3
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-10-08 09:43:22 +0200

    make gradeinfo scrollable, fix problem with jumping animation in accordion

--- polymorphia-frontend/animations/Accordion.tsx
+++ polymorphia-frontend/animations/Accordion.tsx
@@ -1,23 +1,59 @@
 import { gsap } from "gsap";
 
+let previousHeight = 0;
+
 export const openAccordion = (element: HTMLElement, isInstant: boolean) => {
-  gsap.to(element, {
-    height: "auto",
-    opacity: 1,
-    marginTop: "0.5rem",
-    duration: isInstant ? 0.0 : 0.4,
-    ease: "power2.inOut",
-  });
+  // Account for some rounding issues that make accordion jump during animation
+  let height = element.scrollHeight;
+  if (Math.abs(previousHeight - height) <= 1) {
+    height = Math.max(height, previousHeight);
+  }
+  previousHeight = height;
+
+  gsap.fromTo(
+    element,
+    {
+      height: 0,
+      opacity: 0,
+      marginTop: 0,
+    },
+    {
+      height,
+      opacity: 1,
+      marginTop: "0.5rem",
+      duration: isInstant ? 0 : 0.4,
+      ease: "power2.inOut",
+      onComplete: () => {
+        gsap.set(element, { height: "auto" });
+      },
+    }
+  );
 };
 
 export const closeAccordion = (element: HTMLElement, isInstant: boolean) => {
-  gsap.to(element, {
-    height: 0,
-    opacity: 0,
-    marginTop: 0,
-    duration: isInstant ? 0.0 : 0.4,
-    ease: "power2.inOut",
-  });
+  // Account for some rounding issues that make accordion jump during animation
+  let height = element.clientHeight;
+  const marginTop = element.style.marginTop;
+  if (Math.abs(previousHeight - height) <= 1) {
+    height = Math.max(height, previousHeight);
+  }
+  previousHeight = height;
+
+  gsap.fromTo(
+    element,
+    {
+      height,
+      opacity: 1,
+      marginTop: marginTop,
+    },
+    {
+      height: 0,
+      opacity: 0,
+      marginTop: 0,
+      duration: isInstant ? 0 : 0.4,
+      ease: "power2.inOut",
+    }
+  );
 };
 
 export const animateAccordion = (
