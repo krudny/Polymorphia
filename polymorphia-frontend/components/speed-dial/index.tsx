633135746e8979a9b7464a2136cd724551766a5c
Kamil Rudny <k.rudny1@gmail.com>
2025-11-08 23:03:17 +0100

    [I-178] Minor

--- polymorphia-frontend/components/speed-dial/index.tsx
+++ polymorphia-frontend/components/speed-dial/index.tsx
@@ -28,8 +28,8 @@ export default function SpeedDial({ speedDialKey }: SpeedDialProps) {
         icon={<span className="material-symbols">add</span>}
         sx={{
           position: "fixed",
-          bottom: isMd ? 6 : 20,
-          right: isMd ? 12 : 20,
+          bottom: isMd ? 6 : 10,
+          right: isMd ? 12 : 4,
           margin: 0,
           ...(isMd ? {} : { zIndex: 40 }),
         }}
