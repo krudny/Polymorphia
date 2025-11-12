2d0b2452f739dc2206d752173ea2f907fce5ea9f
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2025-11-12 20:34:37 +0100

    [OG-125] Refactor
    Co-authored-by: Krzysztof Ligarski <63918941+kligarski@users.noreply.github.com>

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/model/notification/NotificationType.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/model/notification/NotificationType.java
@@ -31,7 +31,7 @@ public enum NotificationType {
 
     private static final class ErrorMessages {
         private static final String GRADABLE_EVENT_NULL_FOR_GRADE = "GradableEvent nie może być null dla powiadomienia o nowej ocenie.";
-        private static final String REWARD_NULL = "Reward nie może być null dla tego typu powiadomienia.";
+        private static final String REWARD_NULL = "Nagroda nie może być pusta dla tego typu powiadomienia.";
 
         private ErrorMessages() {
         }
