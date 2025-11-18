4f62a7aa99e86f2cd7e88abcbb4bd9e51d8cf4a6
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-11-18 19:32:43 +0100

    prevent infinite loop in grading

---
+++ polymorphia-frontend/shared/filter-defaults/index.tsx
@@ -0,0 +1,6 @@
+export const DEFAULT_SEARCH_BY = ["studentName"];
+export const DEFAULT_SORT_BY = ["total"];
+export const DEFAULT_SORT_ORDER_ASC = ["asc"];
+export const DEFAULT_SORT_ORDER_DESC = ["asc"];
+export const DEFAULT_GROUPS = ["all"];
+export const DEFAULT_GRADE_STATUS = ["all"];
