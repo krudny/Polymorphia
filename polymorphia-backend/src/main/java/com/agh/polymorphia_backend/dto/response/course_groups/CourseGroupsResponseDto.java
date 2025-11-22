56077e3c87940ccb0b0d73bf47cb7855839a36f3
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-11-22 13:55:43 +0100

    fetch course groups directly from DB

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/dto/response/course_groups/CourseGroupsResponseDto.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/dto/response/course_groups/CourseGroupsResponseDto.java
@@ -2,6 +2,7 @@ package com.agh.polymorphia_backend.dto.response.course_groups;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
@@ -9,6 +10,7 @@ import lombok.experimental.SuperBuilder;
 @Data
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
 public class CourseGroupsResponseDto extends CourseGroupsShortResponseDto {
     @NotEmpty
     private final String room;
--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/dto/response/course_groups/CourseGroupsShortResponseDto.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/dto/response/course_groups/CourseGroupsShortResponseDto.java
@@ -1,13 +1,17 @@
 package com.agh.polymorphia_backend.dto.response.course_groups;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseGroupsShortResponseDto {
     @NotNull
     private Long id;
