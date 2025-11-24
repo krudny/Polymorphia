70b96e57a1adb3a130d3a00369c6cd43243a0d48
Kamil Rudny <k.rudny1@gmail.com>
2025-11-24 18:46:55 +0100

    [OG-125] Migrations

--- polymorphia-backend/src/main/resources/db/migration/V20__notifications.sql
+++ polymorphia-backend/src/main/resources/db/migration/V20__notifications.sql
@@ -1,7 +1,7 @@
 CREATE TABLE notifications (
                                id BIGSERIAL PRIMARY KEY,
                                user_id BIGINT NOT NULL,
-                               notification_type VARCHAR(50) NOT NULL,
+                               type VARCHAR(50) NOT NULL,
                                created_at TIMESTAMP WITH TIME ZONE NOT NULL,
                                description TEXT NOT NULL
 );
