8f4507875c64dc8f5b9e81dd9aa00d460b60109f
ustankie <ustankiewicz@student.agh.edu.pl>
2025-12-10 09:43:01 +0100

    [OG-137] FIx after merge

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/controller/CourseController.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/controller/CourseController.java
@@ -21,10 +21,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
