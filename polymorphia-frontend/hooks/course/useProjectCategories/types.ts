4ee97735971e5f51aa9bbba010659ccd33b4bff3
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-12-16 08:55:08 +0100

    move new hooks to match new file structure

--- polymorphia-frontend/hooks/course/useProjectCategories/types.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectCategoryWithVariantsResponseDTO } from "@/interfaces/api/project";
-
-export interface UseProjectCategories {
-  data: ProjectCategoryWithVariantsResponseDTO[] | undefined;
-  isLoading: boolean;
-  isError: boolean;
-}
