6d12957a266bc2bcb4d6af1908d72c4be3282cd8
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-12-27 11:24:51 +0100

    minor cleanup

--- polymorphia-frontend/components/speed-dial/modals/group-info/student-info/StudentInfo.tsx
+++ polymorphia-frontend/components/speed-dial/modals/group-info/student-info/StudentInfo.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { useProjectGroup } from "@/hooks/course/projects/useProjectGroup";
 
 export default function StudentInfo() {
-  const { data, isLoading, isError } = useProjectGroup();
+  const { data: projectGroupStudents, isLoading, isError } = useProjectGroup();
 
   if (isLoading) {
     return (
@@ -15,19 +15,19 @@ export default function StudentInfo() {
       </div>
     );
   }
-  if (isError || data === undefined) {
+  if (isError || !projectGroupStudents) {
     return (
       <ErrorComponent message="Nie udało się załadować informacji o składzie grupy." />
     );
   }
 
-  if (data.length === 0) {
+  if (projectGroupStudents.length === 0) {
     return <ErrorComponent message="Nie należysz do żadnej grupy." />;
   }
 
   return (
-    <div className="flex flex-col gap-2 min-w-80">
-      {data?.map((student) => {
+    <div className="gradable-event-section">
+      {projectGroupStudents.map((student) => {
         const { animalName, fullName, evolutionStage, imageUrl } =
           student.userDetails;
 
