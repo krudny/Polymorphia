29165f7753172f290da59bd14e1e99fef31063db
Kamil Rudny <k.rudny1@gmail.com>
2026-07-18 13:57:26 +0200

    [OG-154] added cors

--- polymorphia-code-executor/build.gradle.kts
+++ polymorphia-code-executor/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
