8ce2de69cd8909c6c633890cc6930c7f6d965fa9
Kamil Rudny <k.rudny1@gmail.com>
2026-08-02 21:25:52 +0200

    [OG-157] Task service

--- polymorphia-frontend/services/tasks/index.ts
+++ polymorphia-frontend/services/tasks/index.ts
@@ -1,40 +1,37 @@
-// TODO: to refactor
-
 import { ApiClient } from "@/services/api/client";
 import {
   ExecuteRequestDTO,
-  ExecuteResponseDTO,
+  ExecuteTaskResponseDTO,
+  SubmitTaskResponseDTO,
   TaskDetailsResponseDTO,
+  TaskSubmissionStatusResponseDTO,
 } from "@/interfaces/api/tasks/types";
 
 const TaskService = {
-  getTaskDetails: async (
-    taskId: number
-  ): Promise<TaskDetailsResponseDTO> => {
+  getTaskDetails: async (taskId: number): Promise<TaskDetailsResponseDTO> => {
     return ApiClient.get(`/tasks/${taskId}`);
   },
 
   runTask: async (
     taskId: number,
-    payload: ExecuteRequestDTO
-  ): Promise<ExecuteResponseDTO> => {
-    return ApiClient.post(`/tasks/${taskId}/run`, payload);
+    request: ExecuteRequestDTO
+  ): Promise<ExecuteTaskResponseDTO> => {
+    return ApiClient.post(`/tasks/${taskId}/run`, request);
   },
-  // submitTask: async (
-  //     taskId: number,
-  //     payload: ExecuteTaskRequestDTO
-  // ): Promise<SubmitTaskResponseDTO> => {
-  //   return ApiClient.post(`/tasks/${taskId}/submissions`, payload);
-  // },
-  // getSubmissionStatus: async (
-  //     taskId: number,
-  //     submissionId: number
-  // ): Promise<TaskSubmissionStatusResponseDTO> => {
-  //   return ApiClient.get(
-  //       `/tasks/${taskId}/submissions/${submissionId}`
-  //   );
-  // },
+
+  submitTask: async (
+    taskId: number,
+    request: ExecuteRequestDTO
+  ): Promise<SubmitTaskResponseDTO> => {
+    return ApiClient.post(`/tasks/${taskId}/submissions`, request);
+  },
+
+  getSubmissionStatus: async (
+    taskId: number,
+    submissionId: number
+  ): Promise<TaskSubmissionStatusResponseDTO> => {
+    return ApiClient.get(`/tasks/${taskId}/submissions/${submissionId}`);
+  },
 };
 
 export default TaskService;
-
