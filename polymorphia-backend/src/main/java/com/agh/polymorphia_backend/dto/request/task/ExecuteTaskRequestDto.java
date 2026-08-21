7bc9e872671081cae7b38c5e360c6c26d0033c4b
Kamil Rudny <k.rudny1@gmail.com>
2026-08-22 00:13:23 +0200

    [OG-155] Validation message

--- polymorphia-backend/src/main/java/com/agh/polymorphia_backend/dto/request/task/ExecuteTaskRequestDto.java
+++ polymorphia-backend/src/main/java/com/agh/polymorphia_backend/dto/request/task/ExecuteTaskRequestDto.java
@@ -3,6 +3,7 @@ package com.agh.polymorphia_backend.dto.request.task;
 import com.agh.polymorphia_backend.model.gradable_event.subtypes.task.TaskSupportedLanguage;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,5 +14,6 @@ public class ExecuteTaskRequestDto {
     private TaskSupportedLanguage taskLanguage;
 
     @NotBlank
+    @Size(max = 64000, message = "Kod źródłowy jest zbyt długi.")
     private String sourceCode;
 }
